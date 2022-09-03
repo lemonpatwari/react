989e6716aba522e95ff54edeaad0442fc5dd7baa
Lemon Patwari <lemonpatwari@gmail.com>
2022-09-03 23:43:26 +0600

    episod 2 a better dev exparience with (react snipet)

--- src/Another.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Another(props){
-    return (
-        <h1>working......{props.name}</h1>
-    )
-}
---
+++ src/Another.jsx
@@ -0,0 +1,16 @@
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+
+class Another extends PureComponent {
+    render() {
+        return (
+            <div>
+                <h1>working....</h1>
+            </div>
+        );
+    }
+}
+
+Another.propTypes = {};
+
+export default Another;
