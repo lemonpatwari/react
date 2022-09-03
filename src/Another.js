11d36627a3f45802f9365ae29b527a9473e92a4c
Lemon Patwari <lemonpatwari@gmail.com>
2022-09-03 22:58:36 +0600

    episod 1

---
+++ src/Another.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Another(props){
+    return (
+        <h1>working......{props.name}</h1>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import Another from "./Another";
 function App() {
+    const [count,setCount] = useState(0);
+    function decrement(){
+        setCount(count=>count-1)
+    }
+    function increment(){
+        setCount(count=>count+1)
+    }
     return (
         <div className="App">
             <header className="App-header">
+
+                <Another name='react'/>
+                <p>{count}</p>
+
+                <button onClick={decrement}>-</button>
+                <button onClick={increment}>+</button>
+
+
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
